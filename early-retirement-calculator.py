c119047c31ac234472c1fec9632716492de0ddeb
mieubrisse <mieubrisse@gmail.com>
2020-04-19 22:32:32 -0300

    Mid-refactoring early retirement calculator

--- early-retirement-calculator.py
+++ early-retirement-calculator.py
@@ -73,12 +73,12 @@ for years_out_str, contrib_str, contrib_rate_str in parsed_args[CONTRIB_CHANGE_K
         sys.exit(1)
     contrib_changes[years_out] = (contrib, contrib_rate)
 
-class ContributionCalculator:
+class ContributionFunction:
     """
-    Class that returns the yearly contribution amount for any given year in the future,
+    Function that returns the yearly contribution amount for any given year in the [0, years_to_live),
      taking into account contrib increases and changes.
     """
-    def __init__(self, manual_contrib_changes, initial_contrib_amount, initial_contrib_rate):
+    def __init__(self, years_to_live, manual_contrib_changes, initial_contrib_amount, initial_contrib_rate):
         self.contribs = []
         current_base_contrib = initial_contrib_amount
         current_contrib_rate = initial_contrib_rate
@@ -94,60 +94,75 @@ class ContributionCalculator:
     def get_contrib_for_year(self, years_in_future):
         return self.contribs[years_in_future]
 
+class NoRetirementNetWorthFunction:
+    """
+    Function that returns net worth for a given year in range [0, years_to_live), accounting for net worth and
+     contribution changes.
+    """
+    def __init__(self, years_to_live, manual_net_worth_changes, current_retirement_savings, pre_retirement_growth_rate, contribution_function):
+        # TODO make this a nice math formula
+        # We assume:
+        # 1. This money is withdrawn at the start of the year, with the 0th entry being how much you'd need if you were to retire RIGHT NOW
+        # 2. The amount you'd withdraw to last you this year doesn't have inflation applied (it will only increase the amount of next year)
+        # E.g. if I have 2 years to live, this array will look like [gross_retirement_income, gross_retirement_income * (1 + inflation)]
+        self.net_worth = []
+        for i in range(0, years_to_live):
+            value_to_append = None
+            if i == 0:
+                value_to_append = current_retirement_savings
+            else:
+                # Growth from your balance, and then add your annual contribution afterwards (this is conservative)
+                value_to_append = self.net_worth[i-1] * (1 + pre_retirement_growth_rate) + contribution_function.get_contrib_for_year(i)
+            value_with_net_worth_change = max(0, value_to_append + net_worth_changes.get(i, 0))
+            self.net_worth.append(value_with_net_worth_change)
+
+    def get_worth_for_year(self, years_in_future):
+        return self.net_worth[years_in_future]
+
+class RetirementWithdrawalsFunction:
+    """
+    Describes, for each year in [0, years_to_live), the inflation-adjusted absolute withdrawal amount required to meet the desired net retirement income in today's dollars
+    """
+    def __init__(self, years_to_live, net_retirement_income_todays_dollars, retirement_tax_rate, inflation_rate):
+        gross_retirement_income_todays_dollars = net_retirement_income_todays_dollars / (1.0 - retirement_tax_rate)
+        self.withdrawals = []
+        for i in range(0, years_to_live):
+            # We subract one year from the exponentiation because inflation will only kick in one year from now
+            self.withdrawals.append(gross_retirement_income_todays_dollars * (1.0 + inflation_rate) ** i)
+
+    def get_withdrawal_for_year(self, years_in_future):
+        return self.withdrawals[years_in_future]
+
+class RetirementMinWorthFunction:
+    """
+    Function to describe the minimum worth needed at every year in [0,years_to_live) to not run out of money before dying
+    """
+     def __init__(self, years_to_live, withdrawal_function):
+        # This is a super stupid, but super clear, way to do this
+        min_worth_for_last_x_year = []
+        for i in reversed(range(0, years_to_live)):
+            withdrawal = withdrawal_function.get_withdrawal_for_year(i)
+            value_to_append = None
+            if i == 0:
+                value_to_append = withdrawal
+            else:
+                # Our bank account can be a little lower because we'll get in-year growth
+                remaining_balance_needed = min_worth_for_last_x_year[i - 1] / (1 + growth_rate_in_retirement)
+                value_to_append = withdrawal + remaining_balance_needed
+            min_worth_for_last_x_year.append(value_to_append)
+
+        # Necessary to reverse so this makes sense
+        self.min_worth = list(reversed(min_worth_for_last_x_year))
+
+    def get_min_worth_for_year(years_in_future):
+        return self.min_worth[years_in_future]
+
+
 # =============== Main Code ====================================
-
-
-gross_retirement_income_todays_dollars = desired_net_retirement_income_todays_dollars / (1.0 - retirement_tax_rate)
-
-def calculate_balance_after_working_year(previous_balance, pre_retirement_growth_rate, annual_contribution):
-    return previous_balance * (1 + pre_retirement_growth_rate) + annual_contribution
-
-
-# TODO make this a nice math formula
-# We assume:
-# 1. This money is withdrawn at the start of the year, with the 0th entry being how much you'd need if you were to retire RIGHT NOW
-# 2. The amount you'd withdraw to last you this year doesn't have inflation applied (it will only increase the amount of next year)
-# E.g. if I have 2 years to live, this array will look like [gross_retirement_income, gross_retirement_income * (1 + inflation)]
-retirement_withdrawal_in_x_years = []
-for i in range(0, years_to_live):
-    # We subract one year from the exponentiation because inflation will only kick in one year from now
-    retirement_withdrawal_in_x_years.append(gross_retirement_income_todays_dollars * (1.0 + inflation_rate) ** i)
-
-# This is a super stupid, but super clear, way to do this
-bank_account_needed_for_last_x_year = []
-for i, elem in enumerate(reversed(retirement_withdrawal_in_x_years)):
-    value_to_append = None
-    if i == 0:
-        value_to_append = elem
-    else:
-        # Our bank account can be a little lower because we'll get in-year growth
-        remaining_balance_needed = bank_account_needed_for_last_x_year[i - 1] / (1 + growth_rate_in_retirement)
-        value_to_append = elem + remaining_balance_needed
-    bank_account_needed_for_last_x_year.append(value_to_append)
-
-# Necessary to reverse so this makes sense
-bank_account_needed_if_retiring_in_x_years = list(reversed(bank_account_needed_for_last_x_year))
-
-# Assumes you constantly contribute and grow forever
-contrib_calculator = ContributionCalculator(contrib_changes, annual_contribution, annual_contribution_increase_rate)
-no_retirement_bank_account = []
-for i in range(0, years_to_live):
-    value_to_append = None
-    if i == 0:
-        value_to_append = current_retirement_savings
-    else:
-        # Growth from your balance, and then add your annual contribution afterwards (this is conservative)
-        value_to_append = calculate_balance_after_working_year(
-            no_retirement_bank_account[i - 1],
-            pre_retirement_growth_rate,
-            contrib_calculator.get_contrib_for_year(i)
-        )
-    value_with_net_worth_change = max(0, value_to_append + net_worth_changes.get(i, 0))
-    no_retirement_bank_account.append(value_with_net_worth_change)
 
 years_to_retirement = None
 for i in range(0, years_to_live):
-    if no_retirement_bank_account[i] >= bank_account_needed_if_retiring_in_x_years[i]:
+    if no_retirement_worth_function.get_worth_for_year(i) >= min_worth_function.get_min_worth_for_year(i):
         years_to_retirement = i
         break
 
@@ -172,14 +187,14 @@ for i in range(0, years_to_live):
     account_value = None
     withdrawal_value = None
     if i == 0:
-        account_value = no_retirement_bank_account[0]
+        account_value = no_retirement_worth_function.get_worth_for_year(0)
         if i >= years_to_retirement:
             withdrawal_value = retirement_withdrawal_in_x_years[i]
         else:
             withdrawal_value = 0
     # Up to and including first retirement year
     elif i <= years_to_retirement:
-        account_value = no_retirement_bank_account[i]
+        account_value = no_retirement_worth_function.get_worth_for_year(i)
 
         # Before retirement
         if i < years_to_retirement:
@@ -204,3 +219,55 @@ print("years_from_now bank_statement retirement_limit withdrawal")
 print("NOTE: withdrawal is taken out at the start of the year, i.e. immediately after the bank_statement")
 for i in range(0, years_to_live):
     print("%s %s %s %s" % (i, int(final_bank_account[i]), int(bank_account_needed_if_retiring_in_x_years[i]), int(withdrawal[i])))
+
+class RetirementAgeCalculator:
+    """
+    Main class, used to calculate the earliest age of retirement given the various inputs
+    """
+    def __init__(self,
+            current_retirement_savings,
+            annual_contribution,
+            current_retirement_savings,
+            annual_contribution,
+            annual_contribution_increase_rate,
+            pre_retirement_growth_rate,
+            post_retirement_growth_rate,
+            inflation_rate,
+            years_to_live,
+            desired_net_retirement_income_todays_dollars,
+            retirement_tax_rate,
+            manual_contrib_changes=None,
+            manual_net_worth_changes=None):
+        manual_contrib_changes = manual_contrib_changes if manual_contrib_changes is not None else {}
+        manual_net_worth_changes = manual_net_worth_changes if manual_net_worth_changes is not None else {}
+
+        for years_out, worth_change in manual_net_worth_changes:
+            if years_out < 0 or years_out >= years_to_live:
+                raise ValueError("Invalid net worth change year '%s'; must be in range [0,%s)" % years_out, years_to_live)
+        for years_out, contrib, contrib_rate in manual_contrib_changes:
+            if years_out < 0 or years_out >= years_to_live:
+                raise ValueError("Invalid net worth change year '%s'; must be in range [0,%s)" % years_out, years_to_live)
+        if years_to_live < 1:
+            raise ValueError("Years to live must be >= 1")
+
+        self.withdrawals_function = RetirementWithdrawalsFunction(years_to_live, desired_net_retirement_income_todays_dollars, retirement_tax_rate, inflation_rate)
+        self.min_worth_function = RetirementMinWorthFunction(years_to_live, withdrawals_function)
+
+        contribution_function = ContributionFunction(
+            contrib_changes,
+            annual_contribution,
+            annual_contribution_increase_rate
+        )
+        self.no_retirement_worth_function = NoRetirementNetWorthFunction(years_to_live, net_worth_changes, current_retirement_savings, pre_retirement_growth_rate, contribution_function)
+        self.years_to_live = years_to_live
+
+    def get_earliest_retirement(self):
+        """
+        Get the smallest number of years after which you'll be able to retire, or None if not possible
+        """
+        years_to_retirement = None
+        for i in range(0, self.years_to_live):
+            if self.no_retirement_worth_function.get_worth_for_year(i) >= self.min_worth_function.get_min_worth_for_year(i):
+                years_to_retirement = i
+                break
+        return years_to_retirement
