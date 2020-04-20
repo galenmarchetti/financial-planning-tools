949383e657be038247e9e95b87e348d945180a6c
mieubrisse <mieubrisse@gmail.com>
2020-04-19 21:02:00 -0300

    Bugfix

--- early-retirement-calculator.py
+++ early-retirement-calculator.py
@@ -35,7 +35,7 @@ annual_contribution = parsed_args[ANNUAL_CONTRIB_KEY]
 annual_contribution_increase_rate = parsed_args[ANNUAL_CONTRIB_INCREASE_RATE_KEY]
 
 #  NOTE: this is the growth rate you expect BEFORE retirement! In retirement, we switch to more conservative securities that only get inflation rate
-market_growth_rate = parsed_args[PRE_GROWTH_RATE_KEY]
+pre_retirement_growth_rate = parsed_args[PRE_GROWTH_RATE_KEY]
 growth_rate_in_retirement = parsed_args[POST_GROWTH_RATE_KEY]
 inflation_rate = parsed_args[INFLATION_RATE_KEY]
 
@@ -44,7 +44,8 @@ desired_net_retirement_income_todays_dollars = parsed_args[NET_RETIREMENT_INCOME
 retirement_tax_rate = parsed_args[RETIREMENT_TAX_RATE_KEY]
 
 net_worth_changes = { int(years_out): int(change) for years_out, change in parsed_args[NET_WORTH_CHANGE_KEY] }
-
+contrib_changes = { int(years_out): (int(contrib), float(contrib_rate)) for years_out, contrib, contrib_rate in parsed_args[CONTRIB_CHANGE_KEY] }
+# TODO use this^^^
 
 
 # =============== Main Code ====================================
@@ -52,8 +53,8 @@ net_worth_changes = { int(years_out): int(change) for years_out, change in parse
 
 gross_retirement_income_todays_dollars = desired_net_retirement_income_todays_dollars / (1.0 - retirement_tax_rate)
 
-def calculate_balance_after_working_year(previous_balance, market_growth_rate, annual_contribution):
-    return previous_balance * (1 + market_growth_rate) + annual_contribution
+def calculate_balance_after_working_year(previous_balance, pre_retirement_growth_rate, annual_contribution):
+    return previous_balance * (1 + pre_retirement_growth_rate) + annual_contribution
 
 
 
@@ -80,36 +81,27 @@ for i, elem in enumerate(reversed(retirement_withdrawal_in_x_years)):
     bank_account_needed_for_last_x_year.append(value_to_append)
 
 # Necessary to reverse so this makes sense
-bank_account_needed_if_retiring_in_x_years = bank_account_needed_for_last_x_year[::-1]
+bank_account_needed_if_retiring_in_x_years = list(reversed(bank_account_needed_for_last_x_year))
 
 # Assumes you constantly contribute and grow forever
-hypothetical_bank_account = []
+no_retirement_bank_account = []
 for i in range(0, years_to_live):
     value_to_append = None
     if i == 0:
         value_to_append = current_retirement_savings
     else:
         # Growth from your balance, and then add your annual contribution afterwards (this is conservative)
-            value_to_append = calculate_balance_after_working_year(
-                hypothetical_bank_account[i - 1],
-                market_growth_rate,
-                annual_contribution * (1 + annual_contribution_increase_rate) ** (i - 1)
-            )
+        value_to_append = calculate_balance_after_working_year(
+            no_retirement_bank_account[i - 1],
+            pre_retirement_growth_rate,
+            annual_contribution * (1 + annual_contribution_increase_rate) ** (i - 1)
+        )
     value_with_net_worth_change = max(0, value_to_append + net_worth_changes.get(i, 0))
-    hypothetical_bank_account.append(value_with_net_worth_change)
-
-print(hypothetical_bank_account)
-
-"""
-# TODO Debugging
-print(hypothetical_bank_account)
-print(bank_account_needed_if_retiring_in_x_years)
-sys.exit(99)
-"""
+    no_retirement_bank_account.append(value_with_net_worth_change)
 
 years_to_retirement = None
 for i in range(0, years_to_live):
-    if hypothetical_bank_account[i] >= bank_account_needed_if_retiring_in_x_years[i]:
+    if no_retirement_bank_account[i] >= bank_account_needed_if_retiring_in_x_years[i]:
         years_to_retirement = i
         break
 
@@ -129,41 +121,40 @@ for key in net_worth_changes.keys():
 # Because we're a little cautious in that we'll only say you can retire when your bank account is >= the amount you'd need for the rest of your life, re-calculate to get exact values of your bank account after you retire
 # TODO this entire chunk is a bunch of spaghetti code; refactor it
 final_bank_account = []
-withdrawal = [] # At start of year
+withdrawal = []
 for i in range(0, years_to_live):
     account_value = None
     withdrawal_value = None
     if i == 0:
-        account_value = hypothetical_bank_account[0]
+        account_value = no_retirement_bank_account[0]
         if i >= years_to_retirement:
             withdrawal_value = retirement_withdrawal_in_x_years[i]
         else:
             withdrawal_value = 0
-    else:
-        last_year_value = final_bank_account[i - 1]
-        if i <= years_to_retirement:
-            account_value = calculate_balance_after_working_year(
-                last_year_value,
-                market_growth_rate,
-                annual_contribution * (1 + annual_contribution_increase_rate) ** (i - 1)
-            )
-            # TODO probably a cleaner way to do that, but not worrying about it for now
-            if i < years_to_retirement:
-                withdrawal_value = 0
-            else:
-                withdrawal_value = retirement_withdrawal_in_x_years[i]
+    # Up to and including first retirement year
+    elif i <= years_to_retirement:
+        account_value = no_retirement_bank_account[i]
+
+        # Before retirement
+        if i < years_to_retirement:
+            withdrawal_value = 0
+        # First retirement year
         else:
-            # To be conservative, we assume you take out your retirement income at the start of the year (i.e. no growth on it)
-            last_year_withdrawal = retirement_withdrawal_in_x_years[i - 1]
-            account_value = max(
-                0,
-                (last_year_value - last_year_withdrawal) * (1 + growth_rate_in_retirement)
-            )
             withdrawal_value = retirement_withdrawal_in_x_years[i]
+    # After first retirement year
+    else:
+        # To be conservative, we assume you take out your retirement income at the start of the year (i.e. no market growth on it)
+        last_year_value = final_bank_account[i - 1]
+        last_year_withdrawal = retirement_withdrawal_in_x_years[i - 1]
+        account_value = max(
+            0,
+            (last_year_value - last_year_withdrawal) * (1 + growth_rate_in_retirement)
+        )
+        withdrawal_value = retirement_withdrawal_in_x_years[i]
     final_bank_account.append(account_value)
     withdrawal.append(withdrawal_value)
 
-print("years_from_now bank_statement withdrawal")
+print("years_from_now bank_statement needed withdrawal")
 print("NOTE: withdrawal is taken out at the start of the year, i.e. immediately after the bank_statement")
 for i in range(0, years_to_live):
-    print("%s %s %s" % (i, int(final_bank_account[i]), int(withdrawal[i])))
+    print("%s %s %s %s" % (i, int(final_bank_account[i]), int(bank_account_needed_if_retiring_in_x_years[i]), int(withdrawal[i])))
